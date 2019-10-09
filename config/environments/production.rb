92882e07b76330d73e30e21090632bcfac29602e
Zach Nusbaum <zach@zdn.dev>
2019-10-09 12:09:46 -0700

    fix email error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.delivery_method = :postmark
-  config.action_mailer.postmark_settings = Rails.application.credentials.postmark.dig(:api_token)
+  config.action_mailer.postmark_settings = {api_token: Rails.application.credentials.postmark.dig(:api_token)}
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
