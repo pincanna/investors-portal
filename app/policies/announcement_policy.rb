c61aec8e7c859e927fa37b7e524cb42a5da3de4b
Zach Nusbaum <zach@zdn.dev>
2019-10-09 15:37:41 -0700

    add more auth

--- app/policies/announcement_policy.rb
+++ app/policies/announcement_policy.rb
@@ -1,4 +1,19 @@
 class AnnouncementPolicy < ApplicationPolicy
+  def show?
+    user.has_role?(:admin_portal)
+  end
+
+  def create?
+    user.has_role?(:announcement_creator)
+  end
+
+  def update?
+    user.has_role?(:announcement_creator)
+  end
+
+  def destroy?
+    user.has_role?(:announcement_creator)
+  end
   class Scope < Scope
     def resolve
       scope.all
