9365d47069693572e2d4d33179d44b961278b5c3
Zach Nusbaum <zach@zdn.dev>
2019-10-09 14:30:33 -0700

    add authorization for documents

--- app/policies/document_policy.rb
+++ app/policies/document_policy.rb
@@ -1,6 +1,20 @@
 class DocumentPolicy < ApplicationPolicy
   def index?
+    user.has_role?(:document_read_all)
   end
+
+  def show?
+    user.has_role?(:document_read_all)
+  end
+
+  def update?
+    user.has_role?(:document_editor)
+  end
+
+  def create?
+    user.has_role?(:document_creator)
+  end
+
   class Scope < Scope
     def resolve
       scope.all
