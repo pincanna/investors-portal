579a6e159be389b708a75b17b264ebe1b0c51507
Zach Nusbaum <zach@zdn.dev>
2019-10-09 16:00:00 -0700

    add user inviter role

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -1,3 +1,9 @@
 class Users::InvitationsController < Devise::InvitationsController
+  before_action :authorize , only: [:new, :create]
 
+  private
+
+  def authorize
+    redirect_to root_url, notice: 'You cannot do that' unless current_user.has_role?(:user_inviter)
+  end
 end
