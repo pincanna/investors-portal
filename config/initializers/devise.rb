4d7c2a17119312f72200343eca96f36d39c981c6
Zach Nusbaum <zach@zdn.dev>
2019-10-09 16:07:19 -0700

    change email address

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -18,7 +18,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = '"Pincanna Investors" <investor-accounts@mg.pincanna.com>'
+  config.mailer_sender = '"Pincanna Investors" <accounts@investors.pincanna.com>'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
