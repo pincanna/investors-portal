7918674db5d726ebabf728a7d127e5f806016a00
Zach Nusbaum <zach@zdn.dev>
2019-10-09 13:39:39 -0700

    add documents to admin panel

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ require 'sidekiq/web'
 
 Rails.application.routes.draw do
   namespace :admin do
+    resources :documents
     resources :users
     resources :announcements
     resources :notifications
