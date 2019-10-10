ade87f1e1515e7b857713c1a72045f735753c166
Zach Nusbaum <zach@zdn.dev>
2019-10-10 13:52:30 -0700

    require sign in

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,6 +1,8 @@
 class DocumentsController < ApplicationController
+  before_action :authenticate_user!
   def index
     @documents = DocumentDecorator.decorate_collection(policy_scope(Document))
+    #authorize @documents
   end
 
   def show
@@ -10,5 +12,6 @@ class DocumentsController < ApplicationController
 
   def set_document
     @document = DocumentDecorator.decorate(policy_scope(Document).find(params[:id]))
+    #authorize @document
   end
 end
