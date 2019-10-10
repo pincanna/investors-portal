767fd88ee2f13aa4ca4a129fcd1ae9a9932879fa
Zach Nusbaum <zach@zdn.dev>
2019-10-10 13:56:17 -0700

    add show page for secure document

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,5 +1,6 @@
 class DocumentsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_document, only: [:show]
   def index
     @documents = DocumentDecorator.decorate_collection(policy_scope(Document))
     #authorize @documents
